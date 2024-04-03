d997b8554a7d71f12696f4e57634f5a7013d1ba9
Theodore Carman <theodore.carman1@gmail.com>
2024-04-03 08:57:47 -0700

    made grid

--- render.js
+++ render.js
@@ -1,9 +1,22 @@
 
+
 // Create Minesweeper Grid
-height = 5
-width = 10
-for (let i = 0; i < height; i++) {
-    for (let i = 0; i < width; i++) {
-        
+document.addEventListener("DOMContentLoaded", function() {
+
+    height = 5;
+    width = 10;
+    grid = document.getElementById("grid");
+
+    for (let i = 0; i < height; i++) {
+        row = document.createElement("div");
+        row.className = "row";
+        console.log(row);
+        console.log(grid);
+        grid.appendChild(row);
+        for (let j = 0; j < width; j++) {
+            cell = document.createElement("div");
+            cell.className = "cell";
+            row.appendChild(cell);
+        }
     }
-}
+});
