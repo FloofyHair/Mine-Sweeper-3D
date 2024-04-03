ada18d9f5c5c9f03db0b853423eed3feeab88a1f
Theodore Carman <theodore.carman1@gmail.com>
2024-04-03 09:19:24 -0700

    Added grid css

--- render.js
+++ render.js
@@ -5,17 +5,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     height = 5;
     width = 10;
+
+    if (height > width) {
+        temp = height;
+        height = width;
+        width = temp;
+    }
+
     grid = document.getElementById("grid");
+
+    grid.style.aspectRatio = width + "/" + height;
 
     for (let i = 0; i < height; i++) {
         row = document.createElement("div");
         row.className = "row";
-        console.log(row);
-        console.log(grid);
         grid.appendChild(row);
         for (let j = 0; j < width; j++) {
             cell = document.createElement("div");
             cell.className = "cell";
+            cell.id = i + "-" + j;
+            cell.addEventListener("click", function() {
+                console.log(cell.id);
+            });
             row.appendChild(cell);
         }
     }
