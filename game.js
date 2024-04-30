c0531e03f539d1319bad9a440d763afc41a89313
Robin Eriksson <robin.kme@gmail.com>
2024-04-30 14:02:13 -0700

    first click always on a 0, cleaned the namespace of game.js

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 const width = 20;
 const height = 10;
-const initialMines = 1;
+const initialMines = 30;
+
 const dX = [1, 1, 0, -1, -1, -1,  0,  1];
 const dY = [0, 1, 1,  1,  0, -1, -1, -1];
-
+const num_flags = 0;
 function randInt(min, max){
     return Math.floor(Math.random() * (max - min)) + min
 }
@@ -27,8 +28,8 @@ function generateBoard(num_mines){
     grid = generateGrid(width, height);
     let i = 0;
     while(i<num_mines){
-        x = randInt(0, width-1);
-        y = randInt(0, height-1);
+        let x = randInt(0, width-1);
+        let y = randInt(0, height-1);
 
         if(grid[y][x] == 0){
             i++;
@@ -42,9 +43,9 @@ function generateBoard(num_mines){
 function getGridNumbers(mine_grid){
     var grid = new Array(height);
 
-    for (var y = 0; y < height; y++) {
+    for (let y = 0; y < height; y++) {
         grid[y] = new Array(width);
-        for (var x = 0; x < width; x++) {
+        for (let x = 0; x < width; x++) {
             grid[y][x] = numNeighbors(mine_grid,x,y)
         }
     }
@@ -62,8 +63,8 @@ function numNeighbors(grid, x, y){
     }
     num = 0
     for(var n = 0; n<8; n++){
-        dx = x+dX[n];
-        dy = y+dY[n];
+        let dx = x+dX[n];
+        let dy = y+dY[n];
         if(validCell(dx,dy)){
             num += grid[dy][dx]
         }
@@ -73,7 +74,7 @@ function numNeighbors(grid, x, y){
 
 function logB(board){
     board.forEach(row => {
-        x = ''
+        let x = ''
         row.forEach(n=>{
             if(n==-1){
                 x+='X'
@@ -97,7 +98,7 @@ function floodFill(x,y, numbers, old_mask){
         let cy = current[1]
         if((!validCell(cx,cy))||(mask[cy][cx]==1)){continue}
         mask[cy][cx] = 1
-        if(numbers[cy][cx]>0){continue}
+        if(numbers[cy][cx]!=0){continue}
         for(let d = 0; d<8; d++){
             queue.push([cx+dX[d],cy+dY[d]])
         }
@@ -110,10 +111,11 @@ function floodFill(x,y, numbers, old_mask){
     return mask
 }
 
-board = generateBoard(20)
-numbers = getGridNumbers(board)
+board = generateGrid(width, height)
+numbers = generateGrid(width, height)
 mask = generateGrid(width, height)
 flags = generateGrid(width, height)
+
 document.addEventListener("DOMContentLoaded", function() {
     createGrid(width, height, numbers);
     updateGrid(width, height, mask, flags, numbers);
@@ -134,12 +136,30 @@ function updateFlags(width, height, flags, mask){
     return newFlags
 }
 firstTime = true;
+
+function onFirstClick(cell){
+    clickPos = getXY(cell.id);
+    firstTime = false;
+    updateMines(initialMines); // Initialize mines
+    incrementTimer(); // Initialize timer
+    // board = generateBoard(initialMines)
+    // numbers = getGridNumbers(board)
+
+
+    let x = clickPos[0]
+    let y = clickPos[1]
+    board = generateBoard(initialMines)
+    while(numNeighbors(board, x, y)!=0){
+        // console.log(board)
+        // console.log(board[0][0])
+        board = generateBoard(initialMines)
+    }
+
+    numbers = getGridNumbers(board)
+}
 function click(cell) {
     if (firstTime) {
-        firstTime = false;
-
-        updateMines(initialMines); // Initialize mines
-        incrementTimer(); // Initialize timer
+        onFirstClick(cell)
     }
     clickPos = getXY(cell.id);
     mask = floodFill(clickPos[0], clickPos[1], numbers, mask);
