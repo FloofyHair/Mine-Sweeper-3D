70a073845a88ef601296636360d185880298e45a
Theodore Carman <theodore.carman1@gmail.com>
2024-05-21 22:20:12 -0700

    reformating

--- menu.js
+++ menu.js
@@ -8,54 +8,55 @@ var gameState = 0;
 // 2 -> win
 // 3 -> loss
 document.addEventListener("DOMContentLoaded", () => {
-  gameSetup(width, height);
+    gameSetup(width, height);
 });
 
 function toggleMenu() {
-  document.getElementById("menu").classList.toggle("toggled");
+    document.getElementById("menu").classList.toggle("toggled");
 }
 
 function getIntVal(elementName) {
-  return parseInt(document.getElementById(elementName).value);
+    return parseInt(document.getElementById(elementName).value);
 }
 
 function formSubmit() {
-  gameState = 0;
-  widthInp = getIntVal("widthInput");
-  heightInp = getIntVal("heightInput");
-  minesInp = getIntVal("minesInput");
+    gameState = 0;
+    widthInp = getIntVal("widthInput");
+    heightInp = getIntVal("heightInput");
+    minesInp = getIntVal("minesInput");
 
-  if (heightInp > widthInp) {
-    [heightInp, widthInp] = [widthInp, heightInp];
-  }
 
-  if (isNaN(widthInp) || isNaN(heightInp) | isNaN(minesInp)) {
-    return;
-  }
-  if (widthInp <= 0 || heightInp <= 0 || minesInp >= widthInp * heightInp) {
-    return;
-  }
-  toggleMenu();
-  width = widthInp;
-  height = heightInp;
-  initialMines = minesInp;
-  timerValue = 0;
-  incrementTimer();
-  gameSetup(width, height);
+    if (isNaN(widthInp) || isNaN(heightInp) | isNaN(minesInp)) {
+        return;
+    }
+    if (widthInp <= 0 || heightInp <= 0 || minesInp >= widthInp * heightInp) {
+        return;
+    }
+    if (heightInp > widthInp) {
+        [heightInp, widthInp] = [widthInp, heightInp];
+    }
+
+    toggleMenu();
+    width = widthInp;
+    height = heightInp;
+    initialMines = minesInp;
+    timerValue = 0;
+    incrementTimer();
+    gameSetup(width, height);
 }
 
 function updateMenu() {
-  if (gameState == 2 || gameState == 3) {
-    toggleMenu();
-  }
+    if (gameState == 2 || gameState == 3) {
+        toggleMenu();
+    }
 
-  if (gameState == 2) {
-    document.getElementById(
-      "feedback-text"
-    ).innerHTML = `You won in ${timerValue} seconds!`;
-  }
+    if (gameState == 2) {
+        document.getElementById(
+            "feedback-text"
+        ).innerHTML = `You won in ${timerValue} seconds!`;
+    }
 
-  if (gameState == 3) {
-    document.getElementById("feedback-text").innerHTML = "You Lose!";
-  }
+    if (gameState == 3) {
+        document.getElementById("feedback-text").innerHTML = "You Lose!";
+    }
 }
